e87efd9d91764c8265e3ae7563f7a77ee85e5333
andre-erpenbeck <58033663+andre-erpenbeck@users.noreply.github.com>
2025-10-30 15:47:34 -0400

    Update test_second_quant_operators.py

--- test_second_quant_operators.py
+++ test_second_quant_operators.py
@@ -1,57 +1,65 @@
+import unittest
 import numpy as np
-import pytest
-from fermionic_operators import FermionicOperators  # adjust if class is in same file
+from second_quant_operators import FermionicOperators  # adjust if class is in the same file
 
-@pytest.mark.parametrize("N", [1, 2, 3])
-def test_operator_shapes(N):
-    f = FermionicOperators(N)
-    for i in range(N):
-        c = f.get_annihilation(i)
-        cd = f.get_creation(i)
-        assert c.shape == (2**N, 2**N)
-        assert cd.shape == (2**N, 2**N)
+class TestFermionicOperators(unittest.TestCase):
+    def test_shapes(self):
+        """Check that operators have the correct dimensions."""
+        for N in [1, 2, 3]:
+            f = FermionicOperators(N)
+            for i in range(N):
+                c = f.get_annihilation(i)
+                cd = f.get_creation(i)
+                self.assertEqual(c.shape, (2**N, 2**N))
+                self.assertEqual(cd.shape, (2**N, 2**N))
 
-def test_hermitian_conjugation():
-    N = 2
-    f = FermionicOperators(N)
-    for i in range(N):
-        c = f.get_annihilation(i)
-        cd = f.get_creation(i)
-        assert np.allclose(cd, c.conj().T)
+    def test_hermitian_conjugation(self):
+        """Verify that c_i^† = c_i† (Hermitian conjugate)."""
+        N = 2
+        f = FermionicOperators(N)
+        for i in range(N):
+            c = f.get_annihilation(i)
+            cd = f.get_creation(i)
+            self.assertTrue(np.allclose(cd, c.conj().T))
 
-def test_anticommutation_relations():
-    N = 3
-    f = FermionicOperators(N)
-    dim = 2**N
-    I = np.eye(dim, dtype=complex)
-    
-    for i in range(N):
-        for j in range(N):
-            ci = f.get_annihilation(i)
-            cj = f.get_annihilation(j)
-            cdi = f.get_creation(i)
-            cdj = f.get_creation(j)
+    def test_anticommutation_relations(self):
+        """Check {c_i, c_j^†} = δ_ij and {c_i, c_j} = 0."""
+        N = 3
+        f = FermionicOperators(N)
+        dim = 2**N
+        I = np.eye(dim, dtype=complex)
+        zero = np.zeros((dim, dim), dtype=complex)
 
-            # {c_i, c_j} = 0
-            assert np.allclose(ci @ cj + cj @ ci, np.zeros((dim, dim)))
+        for i in range(N):
+            for j in range(N):
+                ci, cj = f.get_annihilation(i), f.get_annihilation(j)
+                cdi, cdj = f.get_creation(i), f.get_creation(j)
 
-            # {c_i^†, c_j^†} = 0
-            assert np.allclose(cdi @ cdj + cdj @ cdi, np.zeros((dim, dim)))
+                # {c_i, c_j} = 0
+                self.assertTrue(np.allclose(ci @ cj + cj @ ci, zero))
 
-            # {c_i, c_j^†} = δ_ij
-            lhs = ci @ cdj + cdj @ ci
-            rhs = I if i == j else np.zeros((dim, dim))
-            assert np.allclose(lhs, rhs)
+                # {c_i^†, c_j^†} = 0
+                self.assertTrue(np.allclose(cdi @ cdj + cdj @ cdi, zero))
 
-def test_number_operator():
-    N = 2
-    f = FermionicOperators(N)
-    for i in range(N):
-        c = f.get_annihilation(i)
-        cd = f.get_creation(i)
-        n = cd @ c  # number operator
-        # n^2 = n
-        assert np.allclose(n @ n, n)
-        # Eigenvalues are 0 or 1
-        vals = np.linalg.eigvals(n)
-        assert np.allclose(sorted(set(np.round(vals.real, 8))), [0, 1])
+                # {c_i, c_j^†} = δ_ij
+                lhs = ci @ cdj + cdj @ ci
+                rhs = I if i == j else zero
+                self.assertTrue(np.allclose(lhs, rhs))
+
+    def test_number_operator_properties(self):
+        """Check n_i = c_i^† c_i has correct idempotent behavior."""
+        N = 2
+        f = FermionicOperators(N)
+        for i in range(N):
+            c = f.get_annihilation(i)
+            cd = f.get_creation(i)
+            n = cd @ c
+            # n^2 = n
+            self.assertTrue(np.allclose(n @ n, n))
+            # Eigenvalues should be 0 or 1
+            vals = np.linalg.eigvals(n)
+            rounded_vals = np.unique(np.round(vals.real, 10))
+            self.assertTrue(np.all(v in [0, 1] for v in rounded_vals))
+
+if __name__ == "__main__":
+    unittest.main()
